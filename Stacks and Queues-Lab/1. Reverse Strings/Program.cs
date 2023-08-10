8fe5f7926efb4bdf47348db95a05cecfbb169a4d
EmiliaKrumova <132586170+EmiliaKrumova@users.noreply.github.com>
2023-08-10 23:50:47 +0300

    Add project files.

---
+++ Stacks and Queues-Lab/1. Reverse Strings/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace _1._Reverse_Strings
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           string input = Console.ReadLine();
+            Stack<char> chars = new Stack<char>();
+            for(int i = 0;i<input.Length;i++)
+            {
+                chars.Push(input[i]);
+            }
+            while (chars.Count > 0)
+            {
+                Console.Write(chars.Pop());
+            }
+            Console.WriteLine();
+        }
+    }
+}
---
+++ Stacks and Queues-Lab/2. Stack Sum/Program.cs
@@ -0,0 +1,46 @@
+﻿namespace _2._Stack_Sum
+{
+    
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine().Split(" ",StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+            Stack<int> stack = new Stack<int>(numbers);
+            string command = Console.ReadLine().ToLower();
+           
+            while ((command != "end"))
+            {
+                string[] tokens = command.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+                string realCmd = tokens[0];
+                if(realCmd == "add")
+                {
+                    int firstNumber = int.Parse(tokens[1]);
+                    int secondNumber = int.Parse(tokens[2]);
+                    stack.Push(firstNumber);
+                    stack.Push(secondNumber);
+
+                }
+                else if(realCmd == "remove")
+                {
+                    int elementsToRemove = int.Parse(tokens[1]);
+                    if(stack.Count >= elementsToRemove)
+                    {
+                        for(int i = 0; i < elementsToRemove; i++)
+                        {
+                            stack.Pop();
+                        }
+                    }
+                    else
+                    {
+                        command = Console.ReadLine().ToLower();
+                        continue;
+                    }
+                }
+                command = Console.ReadLine().ToLower();
+            }
+            Console.WriteLine($"Sum: {stack.Sum()}");
+        }
+    }
+}
