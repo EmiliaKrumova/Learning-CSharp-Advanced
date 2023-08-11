f89f67f642a937c5026a0df050cd52496bedcd52
EmiliaKrumova <132586170+EmiliaKrumova@users.noreply.github.com>
2023-08-11 23:35:40 +0300

    added Matching Brackets

--- Stacks and Queues-Lab/4. Matching Brackets/Program.cs
+++ Stacks and Queues-Lab/4. Matching Brackets/Program.cs
@@ -1,10 +1,44 @@
-﻿namespace _4._Matching_Brackets
+﻿using System.Linq.Expressions;
+
+namespace _4._Matching_Brackets
+
+//    We are given an arithmetic expression with brackets.Scan through the string and extract each sub-expression.
+//Print the result back at the terminal.
+
+    //INPUT
+
+//1 + (2 - (2 + 3) * 4 / (3 + 1)) * 5
+
+     //OUTPUT
+
+  //(2 + 3)
+  //(3 + 1)
+  //(2 - (2 + 3) * 4 / (3 + 1))
+
+
 {
     internal class Program
-    {
+{
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            
+            string input = Console.ReadLine();
+            Stack<int> bracketsIndex = new Stack<int>();
+            for(int i = 0; i <input.Length;i++)
+
+            {
+                if (input[i] == '(')
+                {
+                    bracketsIndex.Push(i);
+                }
+                if (input[i] == ')')
+                {
+                    int startIndex = bracketsIndex.Pop();
+                    int endIndex = i;
+                    string substring = input.Substring(startIndex, endIndex-startIndex);
+                    Console.WriteLine(substring);
+                }
+            }
         }
     }
 }
