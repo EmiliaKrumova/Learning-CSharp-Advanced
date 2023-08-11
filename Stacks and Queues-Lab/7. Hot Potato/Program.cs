e063f2b3b13869a2cf93cc95027fddf98646b0ea
EmiliaKrumova <132586170+EmiliaKrumova@users.noreply.github.com>
2023-08-12 01:14:27 +0300

    added Traffic Jam

--- Stacks and Queues-Lab/7. Hot Potato/Program.cs
+++ Stacks and Queues-Lab/7. Hot Potato/Program.cs
@@ -1,5 +1,8 @@
 ﻿namespace _7._Hot_Potato
 {
+    
+    /*Hot potato is a game in which children form a circle and start passing a hot potato. The counting starts with the fist kid. Every nth toss the child left with the potato leaves the game. When a kid leaves the game, it passes the potato along. This continues until there is only one kid left.
+Create a program that simulates the game of Hot Potato.  Print every kid that is removed from the circle. In the end, print the kid that is left last.  */
     internal class Program
     {
         static void Main(string[] args)
---
+++ Stacks and Queues-Lab/8. Traffic Jam/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace _8._Traffic_Jam
+{
+
+    /*Create a program that simulates the queue that forms during a traffic jam. During a traffic jam only N cars can pass the crossroads when the light goes green. Then the program reads the vehicles that arrive one by one and adds them to the queue. When the light goes green N number of cars pass the crossroads and for each a message "{car} passed!" is displayed. When the "end" command is given, terminate the program and display a message with the total number of cars that passed the crossroads.
+     */
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           int carsToPassAtGreenLight = int.Parse(Console.ReadLine());
+            string command;
+            int carsPassed = 0;
+            Queue<string> cars = new Queue<string>();
+            while((command = Console.ReadLine()) != "end")
+            {
+                if (command == "green")
+                {
+                    for (int i = 0;i< carsToPassAtGreenLight; i++)
+                    {
+                        if(cars.Count == 0)
+                        {
+                            break;
+                        }
+                        Console.WriteLine($"{cars.Dequeue()} passed!");
+                        carsPassed++;
+                    }
+                   
+
+                }
+                else
+                {
+                    cars.Enqueue(command);
+                }
+
+            }
+            Console.WriteLine($"{carsPassed} cars passed the crossroads.");
+        }
+    }
+}
