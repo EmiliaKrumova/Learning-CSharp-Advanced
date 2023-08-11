bd2c03ac93537fa0f926ce24d4deb1121bc46521
EmiliaKrumova <132586170+EmiliaKrumova@users.noreply.github.com>
2023-08-12 00:52:45 +0300

    added Hot Potato Game

---
+++ Stacks and Queues-Lab/6. Supermarket/Program.cs
@@ -0,0 +1,31 @@
+﻿using System.Xml.Linq;
+
+namespace _6._Supermarket
+{
+
+    //Reads an input consisting of a name and adds it to a queue until "End" is received.If you receive "Paid", print every customer name and empty the queue, otherwise we receive a client and we have to add him to the queue.When we receive "End" we have to print the count of the remaining people in the queue in the format: "{count} people remaining.".
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string command;
+            Queue<string> names = new Queue<string>();  
+            while((command = Console.ReadLine()) != "End")
+            {
+                if (command == "Paid")
+                {
+                   while (names.Count > 0)
+                    {
+                       Console.WriteLine(names.Dequeue());
+                    }
+                }
+                else
+                {
+                    names.Enqueue(command);
+                }
+
+            }
+            Console.WriteLine($"{names.Count} people remaining.");
+        }
+    }
+}
---
+++ Stacks and Queues-Lab/7. Hot Potato/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace _7._Hot_Potato
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] names = Console.ReadLine().Split();
+            int tosses = int.Parse(Console.ReadLine());
+            Queue<string> kids = new Queue<string>(names);
+            while(kids.Count > 1)
+            { 
+                for(int i = 1;i<=tosses;i++)
+                {
+                    string currentKid = kids.Dequeue();
+                    if (i != tosses)
+                    {
+                        kids.Enqueue(currentKid);
+                    }
+                    else
+                    {
+                        Console.WriteLine($"Removed {currentKid}");
+                    }
+                }
+
+            }
+            Console.WriteLine($"Last is {kids.Dequeue()}");
+        }
+    }
+}
