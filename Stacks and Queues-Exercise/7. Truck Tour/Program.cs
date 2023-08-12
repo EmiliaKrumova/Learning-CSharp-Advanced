d9bbe8b9aa4cc653054f7dfdda3b64b6d66178c1
EmiliaKrumova <132586170+EmiliaKrumova@users.noreply.github.com>
2023-08-13 02:22:05 +0300

    added Truck Tour

---
+++ Stacks and Queues-Exercise/7. Truck Tour/Program.cs
@@ -0,0 +1,72 @@
+﻿namespace _7._Truck_Tour
+{
+    /*
+     Let’s suppose there is a circular route for lorries. There are N petrol pumps on that route. Petrol pumps are 
+numbered 0 to (N−1) (both inclusive). You will receive information (array), corresponding to each of the petrol 
+pumps: [0] the amount of petrol (in litres) that particular petrol pump will give, and [1] the distance(in kilometers) 
+from the current petrol pump to the next petrol pump.
+You have a tank of infinite capacity carrying no petrol. Your task is to figure out, which is the first possible petrol 
+pump, from which the lorry will be able to complete the route. Consider that the lorry will stop at each of the petrol 
+pumps. The lorry will move one kilometer for each liter of petrol.
+
+Input
+ The first line will contain the value of N
+ The next N lines will contain a pair of integers each, i.e. the amount of petrol that petrol pump will give and 
+the distance between that petrol pump and the next petrol pump.
+
+Output
+ An integer which will be the smallest index of the petrol pump from which we can start the tour.
+     */
+
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int countOfPetrolStations = int.Parse(Console.ReadLine());
+            Queue<int[]> petrolStationInfo = new Queue<int[]>();
+
+            for (int i = 0; i < countOfPetrolStations; i++)
+            {
+                int[] station = Console.ReadLine().Split().Select(int.Parse).ToArray();
+                petrolStationInfo.Enqueue(station);
+            }
+            int indexOfStation = 0;
+
+            while (true)
+            {
+                bool isComplete = true;
+                int litres = 0;
+                int[] currStation = petrolStationInfo.Peek();
+
+                foreach (var station in petrolStationInfo)
+                {
+                    int currLitres = station[0];
+                    int currDistance = station[1];
+                    litres += currLitres;
+
+                    if (litres < currDistance)
+                    {
+                        petrolStationInfo.Dequeue();
+                        petrolStationInfo.Enqueue(currStation);
+                        indexOfStation++;
+                        isComplete = false;
+                        break;
+                    }
+                    else
+                    {
+
+                        litres -= currDistance;
+                    }
+                }
+
+                if (isComplete == true)
+                {
+                    Console.WriteLine(indexOfStation);
+                    break;
+                }
+
+            }
+        }
+    }
+}
