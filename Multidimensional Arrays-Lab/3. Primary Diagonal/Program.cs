0d108fb55af6f593f3675f8fa0a403113c7983a6
EmiliaKrumova <132586170+EmiliaKrumova@users.noreply.github.com>
2023-09-19 11:56:14 +0300

    sum of primary diagonal

---
+++ Multidimensional Arrays-Lab/3. Primary Diagonal/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace _3._Primary_Diagonal
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int size = int.Parse(Console.ReadLine());
+            int[,] matrix = new int[size,size];
+            int sumOfDiagonal = 0;
+            for (int row = 0; row < size; row++)
+            {
+                int[] currRow = Console.ReadLine().Split(" ",StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+                
+                for(int col = 0; col < size; col++)
+                {
+                    matrix[row,col] = currRow[col];
+                    if (row == col)
+                    {
+                        sumOfDiagonal += currRow[col];
+                    }
+                }
+
+            }
+            Console.WriteLine(sumOfDiagonal);
+        }
+    }
+}
