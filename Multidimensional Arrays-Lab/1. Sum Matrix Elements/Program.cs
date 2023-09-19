595afecec3a30d4946d2dd97e528c28ef2c624d8
EmiliaKrumova <132586170+EmiliaKrumova@users.noreply.github.com>
2023-09-19 11:32:14 +0300

    added Matrix-Lab

---
+++ Multidimensional Arrays-Lab/1. Sum Matrix Elements/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace _1._Sum_Matrix_Elements
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] dimensions = Console.ReadLine().Split(", ").Select(int.Parse).ToArray();
+            int rows = dimensions[0];
+            int cols = dimensions[1];
+            int[,] matrix = new int[rows, cols];
+            int sum = 0;
+            for (int i = 0; i < rows; i++)
+            {
+                int[] currentRow = Console.ReadLine().Split(", ").Select(int.Parse).ToArray();
+                for (int j = 0; j < cols; j++)
+                {
+                    matrix[i, j] = currentRow[j];
+                    sum += currentRow[j];
+                }
+            }
+            Console.WriteLine(matrix.GetLength(0));
+            Console.WriteLine(matrix.GetLength(1));
+            Console.WriteLine(sum);
+        }
+    }
+}
