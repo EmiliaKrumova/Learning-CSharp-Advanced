5f816867ec41a0dabd860670f9229e9857705ad3
EmiliaKrumova <132586170+EmiliaKrumova@users.noreply.github.com>
2023-09-29 10:12:17 +0300

    added pascal triangle

---
+++ Multidimensional Arrays-Lab/6. Jagged-Array Modification/Program.cs
@@ -0,0 +1,66 @@
+﻿namespace _6._Jagged_Array_Modification
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int rows = int.Parse(Console.ReadLine());
+            int[][] jaggedArr = new int[rows][];
+            for(int row =  0; row < rows; row++)
+            {
+                int[]currRow = Console.ReadLine().Split(" ",StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+                jaggedArr[row] = new int[currRow.Length];// tuk ne sym sigurna
+                for(int col = 0; col < currRow.Length; col++)
+                {
+                    jaggedArr[row][col] = currRow[col];
+                }
+            }
+            string command;
+            while ((command = Console.ReadLine()) != "END")
+            {
+                string[] commandArgs = command.Split(" ", StringSplitOptions.RemoveEmptyEntries).ToArray();
+                string realCom = commandArgs[0];
+                int rowToManipulate = int.Parse(commandArgs[1]);
+                int colToManipulate = int.Parse(commandArgs[2]);
+                int value = int.Parse(commandArgs[3]);
+                bool isValid = false;
+                isValid = Validation(rows, jaggedArr, rowToManipulate, colToManipulate, isValid);
+                if (isValid)
+                {
+                    if (realCom == "Add")
+                    {
+                        jaggedArr[rowToManipulate][colToManipulate] += value;
+                    }
+                    if (realCom == "Subtract")
+                    {
+                        jaggedArr[rowToManipulate][colToManipulate] -= value;
+                    }
+
+                }
+                else
+                {
+                    Console.WriteLine("Invalid coordinates");
+                    continue;
+                }
+            }
+            for(int row = 0; row < rows; row++)
+            {
+                for(int col = 0; col < jaggedArr[row].Length;col++)
+                {
+                    Console.Write($"{jaggedArr[row][col]} ");
+                }
+                Console.WriteLine();
+            }
+        }
+
+        private static bool Validation(int rows, int[][] jaggedArr, int rowToManipulate, int colToManipulate, bool isValid)
+        {
+            if (rowToManipulate >= 0 && rowToManipulate < rows && colToManipulate >= 0 && colToManipulate < jaggedArr[rowToManipulate].Length)
+            {
+                isValid = true;
+            }
+
+            return isValid;
+        }
+    }
+}
---
+++ Multidimensional Arrays-Lab/7. Pascal Triangle/Program.cs
@@ -0,0 +1,40 @@
+﻿namespace _7._Pascal_Triangle
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            long rows = long.Parse(Console.ReadLine());
+            long[][] pascal = new long[rows][];// инициализираме назъбен масив с броя на редовете
+            pascal[0] = new long[1]// инициализираме нулевия ред със стойност 1(единица)
+            {1};
+            for(long row = 1; row < rows; row++)// цикъл от първия ред до броя на редовете
+            {
+                pascal[row] = new long[row + 1];// за всеки нов ред, инициализираме нов масив с дължина номера на реда +1 (първи ред е с 2 колони, втори ред е с 3 колони...)
+                for(long col = 0; col < pascal[row].Length; col++)
+                {
+                    if (col < pascal[row-1].Length)// проверка дали не е на последната колона
+                    {
+                        pascal[row][col] += pascal[row - 1][col];// добавяме горното число
+                    }
+                    
+                    if(col>0)// проверка да не е на първата колона
+                    {
+                        pascal[row][col] += pascal[row - 1][col - 1];// прибавяме числото в ляво от горното
+                    }
+                   
+                }
+            }
+
+            // Отпечатване на назъбен масив
+            for (long row = 0; row < rows; row++)
+            {
+                for (long col = 0; col < pascal[row].Length; col++)
+                {
+                    Console.Write($"{pascal[row][col]} ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
