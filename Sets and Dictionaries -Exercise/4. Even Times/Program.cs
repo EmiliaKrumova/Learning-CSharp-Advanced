0f36e25cc0cf8241d7f04bf7a60d7bb8a2cc8dc8
EmiliaKrumova <132586170+EmiliaKrumova@users.noreply.github.com>
2023-10-02 00:09:54 +0300

    Update Program.cs

--- Sets and Dictionaries -Exercise/4. Even Times/Program.cs
+++ Sets and Dictionaries -Exercise/4. Even Times/Program.cs
@@ -1,4 +1,13 @@
-﻿namespace _4._Even_Times
+﻿
+using System.Collections.Generic;
+
+namespace _4._Even_Times
+
+//    Create a program that prints a number from a collection, which appears an even number of times in it.On the first
+//line, you will be given n – the count of integers you will receive.On the next n lines, you will be receiving the numbers.
+//It is guaranteed that only one of them appears an even number of times. Your task is to find that number and print
+//it in the end. 
+
 {
     internal class Program
     {
