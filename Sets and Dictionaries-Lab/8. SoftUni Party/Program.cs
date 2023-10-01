0d848bd0e589f3de9a4d595e88868e48aa8dca74
EmiliaKrumova <132586170+EmiliaKrumova@users.noreply.github.com>
2023-10-01 22:50:54 +0300

    hash set problem

---
+++ Sets and Dictionaries-Lab/8. SoftUni Party/Program.cs
@@ -0,0 +1,65 @@
+﻿namespace _8._SoftUni_Party
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            HashSet<string> vip = new HashSet<string>();
+            HashSet<string> regular = new HashSet<string>();
+            string input;
+            while ((input = Console.ReadLine()) != "PARTY")
+            {
+                char firstSymbol = input[0];
+                if (char.IsDigit(firstSymbol))
+                {
+                    if (!vip.Contains(input))
+                    {
+                        vip.Add(input);
+                    }
+                }
+                else
+                {
+                    if (!regular.Contains(input))
+                    {
+                        regular.Add(input);
+                    }
+                } 
+            }
+            if (input == "PARTY")
+            {
+                while ((input = Console.ReadLine()) != "END")
+                {
+                    char firstSymbol = input[0];
+                    if (char.IsDigit(firstSymbol))
+                    {
+                        if (vip.Contains(input))
+                        {
+                            vip.Remove(input);
+                        }
+                    }
+                    else
+                    {
+                        if (regular.Contains(input))
+                        {
+                            regular.Remove(input);
+                        }
+                    }
+                }
+            }            
+
+            
+            Console.WriteLine(vip.Count+regular.Count);
+            foreach(var guest in vip)
+            {
+                Console.WriteLine(guest);
+            }
+            foreach(var guest in regular)
+            {
+                Console.WriteLine(guest);
+            }
+
+
+
+        }
+    }
+}
