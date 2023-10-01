767ccbdad86566f48256c602796a59af6a05b7dd
EmiliaKrumova <132586170+EmiliaKrumova@users.noreply.github.com>
2023-10-01 03:55:36 +0300

    added Dictionaries and Sets Lab

---
+++ Sets and Dictionaries-Lab/1. Count Same Values in Array/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace _1._Count_Same_Values_in_Array
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double[] input = Console.ReadLine().Split(" ",StringSplitOptions.RemoveEmptyEntries).Select(double.Parse).ToArray();
+            Dictionary<double, int> numbers = new Dictionary<double, int>();
+            for(int i = 0; i < input.Length; i++)
+            {
+                double currNum = input[i];
+                if(!numbers.ContainsKey(currNum))
+                {
+                    numbers[currNum] = 0;
+                }
+                numbers[currNum]++;
+            }
+            foreach(var number in numbers)
+            {
+                Console.WriteLine($"{number.Key} - {number.Value} times");
+            }
+        }
+    }
+}
---
+++ Sets and Dictionaries-Lab/2. Average Student Grades/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace _2._Average_Student_Grades
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int countOfStudents = int.Parse(Console.ReadLine());
+            Dictionary<string,List<decimal>> studentsData = new Dictionary<string,List<decimal>>();
+            for(int i = 0; i < countOfStudents; i++)
+            {
+                string[] input = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+                string studentName = input[0];
+                decimal studentGrade = decimal.Parse(input[1]);
+                if (!studentsData.ContainsKey(studentName))
+                {
+                    studentsData[studentName] = new List<decimal>();
+                }
+                studentsData[studentName].Add(studentGrade);
+            }
+            foreach(var kvp in studentsData)
+            {
+                Console.WriteLine($"{kvp.Key} -> {String.Join(" ",kvp.Value.Select(v=>v.ToString("F2")))} (avg: {kvp.Value.Average():f2})");
+
+                // начин да форматираш с два знака, след десетичната при String.Join!!!
+
+
+                //Console.WriteLine($"{name} -> {string.Join(" ", b[name].Select(n => n.ToString("F2")))} (avg: {b[name].Average():F2})");
+            }
+        }
+    }
+}
