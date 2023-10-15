70f402d7cff55bd6b4aa5f337c9790ddf7de0859
EmiliaKrumova <132586170+EmiliaKrumova@users.noreply.github.com>
2023-10-15 06:22:02 +0300

    SoftUni Parking

--- Defining Classes/PokemonTrainer/Program.cs
+++ Defining Classes/PokemonTrainer/Program.cs
@@ -5,19 +5,19 @@
         static void Main(string[] args)
         {
             string command = string.Empty;
-            // List<Pokemon> pokemons = new List<Pokemon>();
+            
             List<Trainer> trainers = new List<Trainer>();
             while ((command = Console.ReadLine()) != "Tournament")
             {
                 string[] tokens = command.Split(" ", StringSplitOptions.RemoveEmptyEntries);
-                //"{trainerName} {pokemonName} {pokemonElement} {pokemonHealth}"
+                
                 string trainerName = tokens[0];
                 string pokemonName = tokens[1];
                 string pokemonElement = tokens[2];
                 int pokemonHealth = int.Parse(tokens[3]);
                 Pokemon pokemon = new Pokemon(pokemonName, pokemonElement, pokemonHealth);
-                // pokemons.Add(pokemon);
-                if (!trainers.Any(t => t.Name==(trainerName)))
+                
+                if (!trainers.Any(t => t.Name==trainerName))
                 {
                     Trainer newTrainer = new Trainer(trainerName);
                     newTrainer.PokemonColection.Add(pokemon);
