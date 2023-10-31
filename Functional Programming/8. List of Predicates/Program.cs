d85e26bd266c2f2fa361f95a258681117da4952e
EmiliaKrumova <132586170+EmiliaKrumova@users.noreply.github.com>
2023-10-31 16:00:11 +0200

    method returns Predicate

--- Functional Programming/8. List of Predicates/Program.cs
+++ Functional Programming/8. List of Predicates/Program.cs
@@ -6,33 +6,33 @@
         {
             int endOfSequence = int.Parse(Console.ReadLine());
 
-            int[] deviders = Console.ReadLine().Split().Select(int.Parse).ToArray();
-            int[] numbers = new int[endOfSequence];  
+            int[] deviders = Console.ReadLine().Split().Select(int.Parse).ToArray();//колекция от делители
+            int[] numbers = new int[endOfSequence];  // колекция числа
             for(int i = 0; i < endOfSequence; i++)
             {
-                numbers[i] = i+1;
+                numbers[i] = i+1;// пълня масива от числа
 
             }
-            List<Predicate<int>>predicates = new List<Predicate<int>>();
+            List<Predicate<int>>predicates = new List<Predicate<int>>();// списък с предикати
 
-            foreach(int devider in deviders)
+            foreach(int devider in deviders)// за всеки делител в колекцията делители
             {
-                predicates.Add(x=>x%devider==0);
+                predicates.Add(x=>x%devider==0);// добави го, само ако число се дели на него
             }
 
-            foreach(int number in numbers)
+            foreach(int number in numbers)// за всяко число в колекцията
             {
                 bool isDivisible = true;
-                foreach(var predicate in predicates)
+                foreach(var predicate in predicates) // минавам през всички предикати
                 {
-                    if (!predicate(number))
+                    if (!predicate(number))// ако числото не се дели на делителя прекратяваме цикъла и въртим следващото число в горния форийч
                     {
                         isDivisible = false;
                         break;
                     }
                    
                 }
-                if (isDivisible)
+                if (isDivisible)// ако числото е минало през всички делители и все още е делимо(true), го пишем на конзолата
                 {
                     Console.Write($"{number} ");
                 }
---
+++ Functional Programming/9. Predicate Party!/Program.cs
@@ -0,0 +1,66 @@
+﻿namespace _9._Predicate_Party_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> people = Console.ReadLine().Split().ToList();
+
+            
+            string command;
+            while((command = Console.ReadLine()) != "Party!")
+            {
+                string[] tokens = command.Split();
+                string action = tokens[0];
+                string filter = tokens[1];
+                string value = tokens[2];
+
+                if(action == "Remove")
+                {
+                    people.RemoveAll(GetPredicate(filter, value));
+
+                }else if(action == "Double")
+                {
+                    List<string> duplicatedPeople = people.FindAll(GetPredicate(filter, value));
+                    foreach(string person in duplicatedPeople)
+                    {
+                        int index = people.IndexOf(person);
+                        people.Insert(index, person);
+                    }
+                }
+
+
+            }
+            if(people.Count > 0)
+            {
+                Console.WriteLine($"{String.Join(", ", people)} are going to the party!");
+            }
+            else
+            {
+                Console.WriteLine("Nobody is going to the party!");
+            }
+        }
+
+        private static Predicate<string> GetPredicate(string filter, string value)
+        {
+            if(filter == "StartsWith")
+            {
+                return p=>p.StartsWith(value);
+            }
+            
+            else if (filter == "EndsWith")
+            {
+                return p=>p.EndsWith(value);
+            }
+
+            else if(filter == "Length")
+            {
+                return p => p.Length == int.Parse(value);
+            }
+            else
+            {
+                return default;
+            }
+        }
+    }
+}
