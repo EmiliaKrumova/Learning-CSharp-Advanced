0e5f816188c78fa6651b10d41e71c62cc295aadd
EmiliaKrumova <132586170+EmiliaKrumova@users.noreply.github.com>
2023-11-05 01:59:46 +0200

    Task 1

---
+++ Encapsilation-Lab/PersonsInfo/StartUp.cs
@@ -0,0 +1,24 @@
+﻿namespace PersonsInfo
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            int countOfPersons = int.Parse(Console.ReadLine());
+            List<Person> people = new List<Person>();
+            for(int i = 0; i < countOfPersons; i++)
+            {
+                string[] personData = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+                string firstName = personData[0];
+                string lastName = personData[1];
+                int age = int.Parse(personData[2]);
+                Person person = new Person(firstName, lastName, age);
+                people.Add(person);                
+            }
+            people.OrderBy(p => p.FirstName)
+                    .ThenBy(p => p.Age)
+                    .ToList()
+                    .ForEach(p => Console.WriteLine(p.ToString()));
+        }
+    }
+}
